57e11ea63ce157fc9d72238a49e7ad0e11348b10
rattv <56552417+rattv@users.noreply.github.com>
2020-03-05 15:51:24 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,272 @@
+$(document).ready(function() {
+
+
+    /*Кнопки основного назначения*/
+
+    $('.b-blue').click(function() {
+        $('.text-keys').hide();
+        $('.main-keys').show();
+    });
+
+
+    $('.b-blue').click(function() {
+        $('.file-keys').hide();
+        $('.main-keys').show();
+    });
+
+
+    $('.b-blue').click(function() {
+        $('.explore-keys').hide();
+        $('.main-keys').show();
+    });
+
+
+    $('.b-blue').click(function() {
+        $('.window-keys').hide();
+        $('.main-keys').show();
+    });
+
+
+    $('.b-blue').click(function() {
+        $('.dialogwindow-keys').hide();
+        $('.main-keys').show();
+    });
+
+
+    $('.b-blue').click(function() {
+        $('.browser-keys').hide();
+        $('.main-keys').show();
+    });
+
+
+    /*Работа с текстом*/
+
+    $('.b-purple').click(function() {
+        $('.main-keys').hide();
+        $('.text-keys').show();
+    });
+
+
+
+    $('.b-purple').click(function() {
+        $('.file-keys').hide();
+        $('.text-keys').show();
+    });
+
+
+
+    $('.b-purple').click(function() {
+        $('.explore-keys').hide();
+        $('.text-keys').show();
+    });
+
+
+    $('.b-purple').click(function() {
+        $('.window-keys').hide();
+        $('.text-keys').show();
+    });
+
+
+    $('.b-purple').click(function() {
+        $('.dialogwindow-keys').hide();
+        $('.text-keys').show();
+    });
+
+
+
+    $('.b-purple').click(function() {
+        $('.browser-keys').hide();
+        $('.text-keys').show();
+    });
+
+
+    /*Работа с файлами*/
+
+    $('.b-crimson').click(function() {
+        $('.main-keys').hide();
+        $('.file-keys').show();
+    });
+
+
+    $('.b-crimson').click(function() {
+        $('.text-keys').hide();
+        $('.file-keys').show();
+    });
+
+
+    $('.b-crimson').click(function() {
+        $('.explore-keys').hide();
+        $('.file-keys').show();
+    });
+
+
+    $('.b-crimson').click(function() {
+        $('.window-keys').hide();
+        $('.file-keys').show();
+    });
+
+
+    $('.b-crimson').click(function() {
+        $('.dialogwindow-keys').hide();
+        $('.file-keys').show();
+    });
+
+
+    $('.b-crimson').click(function() {
+        $('.browser-keys').hide();
+        $('.file-keys').show();
+    });
+
+
+    /*Работа в проводнике*/
+
+    $('.b-pink').click(function() {
+        $('.main-keys').hide();
+        $('.explore-keys').show();
+    });
+
+
+    $('.b-pink').click(function() {
+        $('.text-keys').hide();
+        $('.explore-keys').show();
+    });
+
+
+    $('.b-pink').click(function() {
+        $('.file-keys').hide();
+        $('.explore-keys').show();
+    });
+
+
+    $('.b-pink').click(function() {
+        $('.window-keys').hide();
+        $('.explore-keys').show();
+    });
+
+
+    $('.b-pink').click(function() {
+        $('.dialogwindow-keys').hide();
+        $('.explore-keys').show();
+    });
+
+
+    $('.b-pink').click(function() {
+        $('.browser-keys').hide();
+        $('.explore-keys').show();
+    });
+
+
+    /*Работа с окнами*/
+
+    $('.b-red').click(function() {
+        $('.main-keys').hide();
+        $('.window-keys').show();
+    });
+
+
+    $('.b-red').click(function() {
+        $('.text-keys').hide();
+        $('.window-keys').show();
+    });
+
+    $('.b-red').click(function() {
+        $('.file-keys').hide();
+        $('.window-keys').show();
+    });
+
+
+    $('.b-red').click(function() {
+        $('.explore-keys').hide();
+        $('.window-keys').show();
+    });
+
+
+    $('.b-red').click(function() {
+        $('.dialogwindow-keys').hide();
+        $('.window-keys').show();
+    });
+
+
+    $('.b-red').click(function() {
+        $('.browser-keys').hide();
+        $('.window-keys').show();
+    });
+
+
+    /*Работа с диалоговыми окнами*/
+
+    $('.b-lightorange').click(function() {
+        $('.main-keys').hide();
+        $('.dialogwindow-keys').show();
+    });
+
+
+    $('.b-lightorange').click(function() {
+        $('.text-keys').hide();
+        $('.dialogwindow-keys').show();
+    });
+
+
+    $('.b-lightorange').click(function() {
+        $('.file-keys').hide();
+        $('.dialogwindow-keys').show();
+    });
+
+
+    $('.b-lightorange').click(function() {
+        $('.explore-keys').hide();
+        $('.dialogwindow-keys').show();
+    });
+
+
+    $('.b-lightorange').click(function() {
+        $('.window-keys').hide();
+        $('.dialogwindow-keys').show();
+    });
+
+
+    $('.b-lightorange').click(function() {
+        $('.browser-keys').hide();
+        $('.dialogwindow-keys').show();
+    });
+
+
+    /*Работа в браузере*/
+
+    $('.b-orange').click(function() {
+        $('.main-keys').hide();
+        $('.browser-keys').show();
+    });
+
+
+    $('.b-orange').click(function() {
+        $('.text-keys').hide();
+        $('.browser-keys').show();
+    });
+
+
+    $('.b-orange').click(function() {
+        $('.file-keys').hide();
+        $('.browser-keys').show();
+    });
+
+
+    $('.b-orange').click(function() {
+        $('.explore-keys').hide();
+        $('.browser-keys').show();
+    });
+
+
+    $('.b-orange').click(function() {
+        $('.window-keys').hide();
+        $('.browser-keys').show();
+    });
+
+
+    $('.b-orange').click(function() {
+        $('.dialogwindow-keys').hide();
+        $('.browser-keys').show();
+    });
+
+
+});
